8573b93b46fab09b08aafd450668294d760329d8
AnhQuan2004 <anhquan200304@gmail.com>
2026-02-11 09:35:05 +0700

    Initial Commit

---
+++ replit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  replit
+//
+//  Created by VBI2 on 11/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ replit/replitApp.swift
@@ -0,0 +1,17 @@
+//
+//  replitApp.swift
+//  replit
+//
+//  Created by VBI2 on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct replitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
